bf77c46903d45dc695bb5f58f007ca09dc3e837c
Swire Admin <swireadmin@cushman.local>
2014-03-20 10:34:48 -0700

    setting up call to egood api and saving to model

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -10,13 +10,18 @@ module.exports = BaseView.extend({
 		if ( !this.init ) {
 			this.init = true;
 			this.model = new StatModel({}, { app : this.app });
+			var model = this.model;
+			this.model.sync('read', model, {
+				success : function(data, res, jqXHR){
+					model.set(data);
+				}
+			});
 			// use this.model.sync
 			// then apply the res to this.model
 		}
 	},
 	handleStats : function ( err, res ) {
 		// make sure you context is correct `this`
-		console.log( arguments );
 		// you will need to rerender to have the new model applied
 		// this.render();
 	}
